3f1ba9b870adc2a825c5be8c0e2807fe98e468fc
Francisco Erlyson Pamplona <77758434+Erlyson-dev@users.noreply.github.com>
2026-06-18 22:51:07 -0300

    Update themeContext.tsx

--- financas-webapp/src/context/themeContext.tsx
+++ financas-webapp/src/context/themeContext.tsx
@@ -1,8 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import type { ReactNode } from 'react';
 
-type Theme = 'light' | 'dark';
-
 interface ThemeContextValue {
   isDark: boolean;
   toggleTheme: () => void;
